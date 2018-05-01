23cd6460032538ef7f7c85fe702a0b0d18d5ba7d
Yan Fengyao <yanfengyao@Yans-MacBook.local>
2018-05-01 13:44:42 -0400

    add facebook login

--- platforms/browser/www/build/0.js
+++ platforms/browser/www/build/0.js
@@ -1,6 +1,6 @@
 webpackJsonp([0],{
 
-/***/ 1031:
+/***/ 1032:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -8,7 +8,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CoachDtPageModule", function() { return CoachDtPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(23);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__coach_dt__ = __webpack_require__(1037);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__coach_dt__ = __webpack_require__(1038);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -38,7 +38,7 @@ var CoachDtPageModule = /** @class */ (function () {
 
 /***/ }),
 
-/***/ 1037:
+/***/ 1038:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -3,58 +3,32 @@ import { NgForm } from "@angular/forms";
 import { LoadingController, AlertController } from "ionic-angular";
 import { Facebook } from '@ionic-native/facebook';
 import { AuthService } from "../../services/auth";
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html'
 })
 export class SigninPage {
-  isLoggedIn:boolean = false;
-  users: any;
-
+ provider:any;
   constructor(private authService: AuthService,
               private loadingCtrl: LoadingController,
-              private alertCtrl: AlertController,private fb : Facebook) {
-                fb.getLoginStatus()
-    .then(res => {
-      console.log(res.status);
-      if(res.status === "connect") {
-        this.isLoggedIn = true;
-      } else {
-        this.isLoggedIn = false;
-      }
-    })
-    .catch(e => console.log(e));
+              private alertCtrl: AlertController,private facebook : Facebook) {
+      this.provider = new firebase.auth.FacebookAuthProvider();
   }
-  login() {
-  this.fb.login(['public_profile', 'user_friends', 'email'])
-    .then(res => {
-      if(res.status === "connected") {
-        this.isLoggedIn = true;
-        this.getUserDetail(res.authResponse.userID);
-      } else {
-        this.isLoggedIn = false;
-      }
-    })
-    .catch(e => console.log('Error logging into Facebook', e));
-}
-logout() {
-  this.fb.logout()
-    .then( res => this.isLoggedIn = false)
-    .catch(e => console.log('Error logout from Facebook', e));
-}
-getUserDetail(userid) {
-  this.fb.api("/"+userid+"/?fields=id,email,name,picture,gender",["public_profile"])
-    .then(res => {
-      console.log(res);
-      this.users = res;
-    })
-    .catch(e => {
-      console.log(e);
-    });
-}
+  FBlogin():Promise<any> {
+    return this.facebook.login(['email'])
+    .then( response => {
+      const facebookCredential = firebase.auth.FacebookAuthProvider
+        .credential(response.authResponse.accessToken);
 
+      firebase.auth().signInWithCredential(facebookCredential)
+        .then( success => { 
+          console.log("Firebase success: " + JSON.stringify(success)); 
+        });
 
+    }).catch((error) => { console.log(error) });
+}
   onSignin(form: NgForm) {
     const loading = this.loadingCtrl.create({
       content: 'Signing you in...'
